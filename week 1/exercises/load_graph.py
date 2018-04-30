95122fc96119344d32625152db69726a87745479
zhengyufan <zhengyufan@wisers.com>
2018-04-30 17:40:39 +0800

    load graph

---
+++ week 1/exercises/load_graph.py
@@ -0,0 +1,21 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# Instantiate the graph
+G1 = nx.Graph()
+# add node/edge pairs
+G1.add_edges_from([(0, 1),
+                   (0, 2),
+                   (0, 3),
+                   (0, 5),
+                   (1, 3),
+                   (1, 6),
+                   (3, 4),
+                   (4, 5),
+                   (4, 7),
+                   (5, 8),
+                   (8, 9)])
+
+# draw the network G1
+g = nx.draw_networkx(G1)
+plt.show(g)
