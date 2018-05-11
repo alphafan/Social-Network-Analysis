7aaa882fbf9d7a2296bffab85ef05aa753bd3fa6
zhengyufan <zhengyufan@wisers.com>
2018-05-11 12:03:42 +0800

    image

--- week 4/test.py
+++ week 4/test.py
@@ -1,5 +1,16 @@
 import networkx as nx
 
-G = nx.karate_club_graph()
+G = nx.Graph()
 
-print(sorted(nx.adamic_adar_index(G), key=lambda x: x[2], reverse=True))
+G.add_edges_from([('A', 'B'), ('A', 'C'), ('B', 'D'), ('C', 'D'),
+                  ('C', 'E'), ('D', 'E'), ('D', 'G'), ('E', 'G'), ('F', 'G')])
+
+for node in G.nodes():
+    G[node]['community'] = 1
+G['A']['community'] = 0
+
+
+g = nx.DiGraph()
+g.add_edges_from([('A', 'B'), ('B', 'C'), ('A', 'C'), ('C', 'A'), ('D', 'C')])
+
+print(list(nx.cn_soundarajan_hopcroft(G)))
